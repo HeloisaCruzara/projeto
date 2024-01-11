bf815bfd3266cd5309f00725c4b8f37a8d340969
Heloisa <hcruzara@hmail.com>
2024-01-11 16:28:23 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle(`light`)
+
+  /*pode usar assim tbm if (html.classList.contains(`light`)) {
+    html.classList.remove(`light`)
+  } else {
+    html.classList.add(`light`)
+  } */
+
+  // pegar a tag img
+  // substiruir a image
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains(`light`)) {
+    img.setAttribute("src", "./assets/newavatar.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
